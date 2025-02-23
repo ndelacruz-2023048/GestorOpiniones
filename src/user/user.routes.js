9413216e245564de830df8806c8f3aa73b12582c
ndelacruz-2023048 <ndelacruz-2023048@kinal.edu.gt>
2025-02-23 13:44:08 -0600

    Se agrego el archivo json, donde se encuentran los request para probar la api de Gestion de Opiniones

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -5,7 +5,7 @@ import { validateUpdateUser } from "../../middlewares/validators.js";
 
 const apiUser = Router()
 
-apiUser.get("/getUsers",validateJwt,getUsers)
+apiUser.get("/getUser",validateJwt,getUsers)
 apiUser.put("/profile_update",validateJwt,validateUpdateUser,updateUser)
 
 export default apiUser
